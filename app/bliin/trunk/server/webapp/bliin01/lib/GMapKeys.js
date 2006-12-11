dd94430c6c885dfa6e04230153038e8fe53f2bf6
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2006-12-11 22:26:50 +0000

    bliin: gmap fixes

---
+++ app/bliin/trunk/server/webapp/bliin01/lib/GMapKeys.js
@@ -0,0 +1,15 @@
+GMAP.addKey('live',
+		'ABQIAAAAD3bxjYK2kuWoA5XU4dh89xSFYVerP0SQAckZ4lP_icglXWi2BhRnLGDchEZNpySXUzDDjzGY6WHp3g',
+		'^https?://bliin.com/bliin01/.*' );
+
+GMAP.addKey('test',
+		'ABQIAAAAD3bxjYK2kuWoA5XU4dh89xRdGwtZM1g-t4Rjn2uWvdo8YTeXNRT8G5UlhdgE1J3gnRd7RyD8oDwzAg',
+		'^https?://test.bliin.com/bliin01/.*' );
+
+GMAP.addKey('suwa',
+		'ABQIAAAAD3bxjYK2kuWoA5XU4dh89xSCq1xGllVKgKplxPLaaDpU9sQCoxRHHlC2xVI3bLNEfMwpF6z7-ev9_A',
+		'^https?://test.geotracing.com/bliin01/.*' );
+
+
+GMAP.loadGoogleMapScript('2.66');
+
