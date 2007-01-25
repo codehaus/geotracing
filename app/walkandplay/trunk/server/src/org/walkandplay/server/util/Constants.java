95db2a837544cf332d036e8d155c765151437e63
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-01-25 16:54:39 +0000

    wnp: first version tour handler

--- app/walkandplay/trunk/server/src/org/walkandplay/server/util/Constants.java
+++ app/walkandplay/trunk/server/src/org/walkandplay/server/util/Constants.java
@@ -1,6 +1,6 @@
 package org.walkandplay.server.util;
 
-public class Constants {
+public interface Constants {
     // tablenames
     public final static String VERSION_TABLE = "version";
     public final static String TOUR_TABLE = "tour";
@@ -12,19 +12,22 @@ public class Constants {
 
     // table fields
     public final static String ID_FIELD = "id";
+	public final static String OWNER_FIELD = "owner";
     public final static String VERSION_FIELD = "version";
     public final static String NAME_FIELD = "name";
     public final static String DESCRIPTION_FIELD = "description";
     public final static String STATE_FIELD = "state";
     public final static String TYPE_FIELD = "type";
     public final static String LOCATION_FIELD = "location";
+	public final static String TAGS_FIELD = "tags";
 
     public final static String INVITATION = "invitation";
     public final static String INVITATION_SEND = "invitation-send";
     public final static String INVITATION_PENDING = "invitation-pending";
     public final static String INVITATION_CONFIRMED = "invitation-confirmed";
 
-    public final static String INTRO_MEDIA = "intro";
-    public final static String OUTRO_MEDIA = "outro";
+    public final static String RELTAG_INTRO = "intro";
+    public final static String RELTAG_OUTRO = "outro";
+	public final static String RELTAG_MEDIUM = "medium";
 
 }
