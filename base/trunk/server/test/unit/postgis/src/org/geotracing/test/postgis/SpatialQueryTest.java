e7dbda87b9b7ccb04a1fe84ac7a461e0e5796e43
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-02-09 16:46:40 +0000

    base: spatial queries in postgis

--- base/trunk/server/test/unit/postgis/src/org/geotracing/test/postgis/SpatialQueryTest.java
+++ base/trunk/server/test/unit/postgis/src/org/geotracing/test/postgis/SpatialQueryTest.java
@@ -11,62 +11,122 @@ import org.postgis.Point;
  */
 public class SpatialQueryTest extends PGTestCase {
 	public static final String EXTENT_POINT_1 = "SRID=4326;POINT(1 1)";
-	public static final String EXTENT_POINT_2 = "SRID=4326;POINT(1 5)";
-	public static final String EXTENT_POINT_3 = "SRID=4326;POINT(3 5)";
-	public static final String EXTENT_POINT_4 = "SRID=4326;POINT(3 1)";
-	public static final String MIDDLE_POINT = "SRID=4326;POINT(2 2)";
-	public static final String[] RECT = {EXTENT_POINT_1, EXTENT_POINT_2, EXTENT_POINT_3, EXTENT_POINT_4};
+	public static final String EXTENT_POINT_2 = "SRID=4326;POINT(1 7)";
+	public static final String EXTENT_POINT_3 = "SRID=4326;POINT(9 1)";
+	public static final String EXTENT_POINT_4 = "SRID=4326;POINT(9 7)";
+	public static final String MIDDLE_POINT = "SRID=4326;POINT(5 4)";
+	public static final String[] RECT = {EXTENT_POINT_1, EXTENT_POINT_2, EXTENT_POINT_3, EXTENT_POINT_4, MIDDLE_POINT};
 
 	public SpatialQueryTest() {
 		super("SpatialQueryTest");
 	}
 
-	public void testPointInExtent() {
+	public void testSelectPointsInBBox() {
 		try {
 
-			// Simple: create and update and commit.
-			for (int i = 0; i < RECT.length; i++) {
-				insertRecord("p" + (i + 1), RECT[i]);
-			}
+			insertTestPoints();
 
-			insertRecord("middle", MIDDLE_POINT);
-			String queryAll = "SELECT * FROM " + SPATIAL_ONE_TABLE_NAME;
-			Record[] records = getFinder().freeQuery(queryAll);
-			// assertEquals("result not equal ", 5, records.length);
+			// get all
+			String queryBBox = "SELECT * from spatialone where point && SetSRID('BOX3D(0 0, 10 10)'::box3d,4326)";
+			Record[] records = getFinder().freeQuery(queryBBox);
+			assertEquals("result not equal ", 5, records.length);
+
+			// get middle
+			queryBBox = "SELECT * from spatialone where point && SetSRID('BOX3D(2 2, 6 5)'::box3d,4326)";
+			info(queryBBox);
+			records = getFinder().freeQuery(queryBBox);
+			assertEquals("result not equal ", 1, records.length);
+		} catch (Throwable t) {
+			failTest("testSelectPointsInBBox: ", t);
+		}
+	}
+
+	public void testSelectPointsInRadius() {
+		try {
+
+			insertTestPoints();
+
+			// Get single near middle point in radius 100km
+			String queryAround = "SELECT * FROM spatialone WHERE distance_sphere(GeomFromText('POINT(5.1 4.1)',4326),point) < 100000";
+			Record[] records = getFinder().freeQuery(queryAround);
+			assertEquals("result not equal ", 1, records.length);
+			assertEquals("result not middle point ", MIDDLE_POINT, records[0].getField("point") + "");
+
+			// Get all in large radius 10000 km
+			queryAround = "SELECT * FROM spatialone WHERE distance_sphere(GeomFromText('POINT(5.1 4.1)',4326),point) < 10000000";
+			info(queryAround);
+			records = getFinder().freeQuery(queryAround);
+			assertEquals("result not equal ", 5, records.length);
+			// assertEquals("result not middle point ", "POINT(5 4)", records[0].getField("point")+"");
+		} catch (Throwable t) {
+			failTest("testSelectPointsInRadius: ", t);
+		}
+	}
+
+
+	public void testSelectNearestPoints() {
+		try {
+			// select * from villages order by distance((select geom from towns where town='mytown'),geom) asc limit 1;
+
+			insertTestPoints();
 
 			// Get middle
-			String queryMiddle = "SELECT name, AsText(point) AS point FROM " + SPATIAL_ONE_TABLE_NAME +
-					" WHERE point && Expand(GeomFromText('POINT(2.1 2.1)',4326), 0.2)"
-				 + " AND Distance(GeomFromText('POINT(2.1 2.1)',4326),point) < 0.2";
+			String queryNearest =
+					"SELECT * FROM spatialone order by distance_sphere(GeomFromText('POINT(6 5)',4326),point) asc limit 3";
+
+			// "SELECT name, AsText(point) AS point FROM " + SPATIAL_ONE_TABLE_NAME +
+			//		" WHERE point && Expand(GeomFromText('POINT(2.1 2.1)',4326), 0.2)"
+			//		+ " AND Distance(GeomFromText('POINT(2.1 2.1)',4326),point) < 0.2";
 			// queryMiddle = "SELECT name,AsText(point) AS point FROM spatialone WHERE point && Expand(GeomFromText('POINT(2 3)',4326), 100) AND Distance(GeomFromText('POINT(2 3)',4326),point) < 100";
 
 			// works linux:
 			// SELECT name,point AS point FROM spatialone WHERE point && Expand(GeomFromText('POINT(2 3)',4326), 100) AND Distance(GeomFromText('POINT(2 3)',4326),point) < 100;
-			records = getFinder().freeQuery(queryMiddle);
+			Record[] records = getFinder().freeQuery(queryNearest);
 
-			System.out.println(queryMiddle);
+			info(queryNearest);
 			for (int i = 0; i < records.length; i++) {
-				System.out.println(records[i].getField("point"));
+				info(records[i].getField("point")+"");
 			}
-
-			 queryMiddle = "SELECT name,AsText(point) AS point FROM " + SPATIAL_ONE_TABLE_NAME +
-					" WHERE point && GeomFromText('BOX3D(1 2, 5 6)'::box3d,4326);";
-			//queryMiddle ="select name,AsText(point) AS point from spatialone where point && GeomFromText('POLYGON((0 0,0 10,10 10,10 0,0 0))', 4326);";
-			queryMiddle ="SELECT * from spatialone where point && SetSRID('BOX3D(1.5 1.5,2.5 2.5)'::box3d,4326);";
-				System.out.println(queryMiddle);
-			records = getFinder().freeQuery(queryMiddle);
-			assertEquals("result not equal ", 1, records.length);
-			PGgeometry geom = (PGgeometry)records[0].getObjectField("point");
-			Point pt = (Point) geom.getGeometry();
-
-			for (int i = 0; i < records.length; i++) {
-				 geom = (PGgeometry)records[i].getObjectField("point");
-				 pt = (Point) geom.getGeometry();
-				System.out.println("pt lon=" + pt.getX() + " lat=" + pt.getY());
-			}
+			assertEquals("result not equal ", 3, records.length);
+			assertEquals("result[0] not " + MIDDLE_POINT, MIDDLE_POINT, records[0].getField("point") + "");
+			assertEquals("result[1] not " + EXTENT_POINT_4, EXTENT_POINT_4, records[1].getField("point") + "");
+			assertEquals("result[2] not " + EXTENT_POINT_3, EXTENT_POINT_3, records[2].getField("point") + "");
 
 		} catch (Throwable t) {
-			failTest("testExtent: ", t);
+			failTest("testSelectNearestPoints: ", t);
+		}
+	}
+
+	public void testSelectNearestPointsInBBox() {
+		try {
+			// select * from villages order by distance((select geom from towns where town='mytown'),geom) asc limit 1;
+
+			insertTestPoints();
+
+			// Get middle
+			String queryNearest =
+					"SELECT * FROM spatialone where point && SetSRID('BOX3D(2 2, 8 6)'::box3d,4326) order by distance_sphere(GeomFromText('POINT(6 5)',4326),point) asc limit 3";
+
+			// "SELECT name, AsText(point) AS point FROM " + SPATIAL_ONE_TABLE_NAME +
+			//		" WHERE point && Expand(GeomFromText('POINT(2.1 2.1)',4326), 0.2)"
+			//		+ " AND Distance(GeomFromText('POINT(2.1 2.1)',4326),point) < 0.2";
+			// queryMiddle = "SELECT name,AsText(point) AS point FROM spatialone WHERE point && Expand(GeomFromText('POINT(2 3)',4326), 100) AND Distance(GeomFromText('POINT(2 3)',4326),point) < 100";
+
+			// works linux:
+			// SELECT name,point AS point FROM spatialone WHERE point && Expand(GeomFromText('POINT(2 3)',4326), 100) AND Distance(GeomFromText('POINT(2 3)',4326),point) < 100;
+			Record[] records = getFinder().freeQuery(queryNearest);
+
+			info(queryNearest);
+			for (int i = 0; i < records.length; i++) {
+				info(records[i].getField("point")+"");
+			}
+			assertEquals("result not equal ", 1, records.length);
+			assertEquals("result[0] not " + MIDDLE_POINT, MIDDLE_POINT, records[0].getField("point") + "");
+			//assertEquals("result[1] not " + EXTENT_POINT_4, EXTENT_POINT_4, records[1].getField("point") + "");
+			//assertEquals("result[2] not " + EXTENT_POINT_3, EXTENT_POINT_3, records[2].getField("point") + "");
+
+		} catch (Throwable t) {
+			failTest("testSelectNearestPointsInBBox: ", t);
 		}
 	}
 
@@ -77,5 +137,13 @@ public class SpatialQueryTest extends PGTestCase {
 		getModifier().insert(record);
 		return record;
 	}
+
+	protected void insertTestPoints() throws Exception {
+		// Simple: create and update and commit.
+		for (int i = 0; i < RECT.length; i++) {
+			insertRecord("p" + (i + 1), RECT[i]);
+		}
+	}
+
 
 }
