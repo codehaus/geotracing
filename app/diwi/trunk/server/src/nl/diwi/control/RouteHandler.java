d969e3aa1d5fa1af92ddda47ff731983fc5c1a28
rlenz <rlenz@fd817712-dc16-0410-9400-acf3549a053d>
2007-02-22 12:05:51 +0000

    diwi: first setup

--- app/diwi/trunk/server/src/nl/diwi/control/RouteHandler.java
+++ app/diwi/trunk/server/src/nl/diwi/control/RouteHandler.java
@@ -6,6 +6,8 @@ package nl.diwi.control;
 
 
 import nl.justobjects.jox.dom.JXElement;
+import nl.diwi.logic.RouteLogic;
+import nl.diwi.util.Constants;
 import org.keyworx.common.log.Log;
 import org.keyworx.common.log.Logging;
 import org.keyworx.oase.api.OaseException;
@@ -18,6 +20,8 @@ import org.keyworx.utopia.core.session.UtopiaResponse;
 import org.keyworx.utopia.core.util.Oase;
 import org.keyworx.amuse.core.Protocol;
 
+import java.util.Vector;
+
 /**
  * Handles all operations related to commenting.
  * <p/>
@@ -26,9 +30,11 @@ import org.keyworx.amuse.core.Protocol;
  * @author Just van den Broecke
  * @version $Id: CommentHandler.java 361 2007-02-05 21:34:58Z just $
  */
-public class RouteHandler extends DefaultHandler {
+public class RouteHandler extends DefaultHandler implements Constants {
 
-	/**
+    public final static String ROUTE_GENERATE_SERVICE = "route-generate";
+
+    /**
 	 * Processes the Client Request.
 	 *
 	 * @param anUtopiaReq A UtopiaRequest
@@ -38,15 +44,77 @@ public class RouteHandler extends DefaultHandler {
 	public UtopiaResponse processRequest(UtopiaRequest anUtopiaReq) throws UtopiaException {
 		Log log = Logging.getLog(anUtopiaReq);
 
-		// Get the service name for the requestä
+		// Get the service name for the request
 		String service = anUtopiaReq.getServiceName();
 		log.trace("Handling request for service=" + service);
 
-		JXElement response = Protocol.createResponse(service);
-		
+		JXElement response;
+		try {
+			if (service.equals(ROUTE_GENERATE_SERVICE)) {
+				response = generateRoute(anUtopiaReq);
+			} else {
+				// May be overridden in subclass
+				response = unknownReq(anUtopiaReq);
+			}
+		} catch (UtopiaException ue) {
+			log.warn("Negative response service=" + service, ue);
+			response = createNegativeResponse(service, ue.getErrorCode(), ue.getMessage());
+		} catch (Throwable t) {
+			log.error("Unexpected error service=" + service, t);
+			response = createNegativeResponse(service, ErrorCode.__6005_Unexpected_error, "Unexpected error in request " + t);
+		}
+
 		// Always return a response
 		log.trace("Handled service=" + service + " response=" + response.getTag());
 		return new UtopiaResponse(response);
+	}
+
+    /*
+        <route-generate-req >
+             <pref name="bos" value="40" type="outdoor-params" />
+             <pref name="heide" value="20" type="outdoor-params" />
+             <pref name="bebouwing" value="10" type="outdoor-params" />
+             <pref name="theme" value="forts" type="theme" />
+             <pref name="activity" value="wandelaar" type="activity" />
+
+        </route-generate-req>
+     */
+    protected JXElement generateRoute(UtopiaRequest anUtopiaReq) throws UtopiaException {
+		JXElement reqElm = anUtopiaReq.getRequestCommand();
+
+        RouteLogic logic = createLogic(anUtopiaReq);
+        Vector routes = logic.generateRoute(anUtopiaReq);
+
+
+        // Create and return response with open comment id.
+		JXElement response = createResponse(ROUTE_GENERATE_SERVICE);
+        response.addChildren(routes);
+
+        return response;
+	}
+
+    /**
+	 * Get user (Person) id from request.
+	 */
+	protected RouteLogic createLogic(UtopiaRequest anUtopiaReq) throws UtopiaException {
+		return new RouteLogic(anUtopiaReq.getUtopiaSession().getContext().getOase());
+	}
+
+    /**
+	 * Default implementation for unknown service request.
+	 * <p/>
+	 * Override this method in extended class for handling additional
+	 * requests.
+	 *
+	 * @param anUtopiaReq A UtopiaRequest
+	 * @return A negative UtopiaResponse.
+	 * @throws org.keyworx.utopia.core.data.UtopiaException
+	 *          Standard Utopia exception
+	 */
+	protected JXElement unknownReq(UtopiaRequest anUtopiaReq) throws UtopiaException {
+		String service = anUtopiaReq.getServiceName();
+		Logging.getLog(anUtopiaReq).warn("Unknown service " + service);
+		return createNegativeResponse(service, ErrorCode.__6000_Unknown_command, "unknown service: " + service);
 	}
 
 }
---
+++ app/diwi/trunk/server/src/nl/diwi/util/Constants.java
@@ -0,0 +1,27 @@
+package nl.diwi.util;
+
+public interface Constants {
+    // tablenames
+    public final static String ROUTE_TABLE = "diwi_route";
+    public final static String PREFS_TABLE = "diwi_prefs";
+
+    // xml tags
+    public final static String PREF_ELM = "pref";
+    public final static String ROUTE_ELM = "route";
+
+    // table fields
+    public final static String ID_FIELD = "id";
+    public final static String OWNER_FIELD = "owner";
+    public final static String VERSION_FIELD = "version";
+    public final static String NAME_FIELD = "name";
+    public final static String DESCRIPTION_FIELD = "description";
+    public final static String STATE_FIELD = "state";
+    public final static String VALUE_FIELD = "value";
+    public final static String PATH_FIELD = "path";
+    public final static String TYPE_FIELD = "type";
+    public final static String STARTDATE_FIELD = "startdate";
+    public final static String ENDDATE_FIELD = "enddate";
+    public final static String PLAYERS_FIELD = "players";
+
+
+}
