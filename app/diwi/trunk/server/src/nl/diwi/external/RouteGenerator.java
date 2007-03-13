9b19931459d7ee301f645408e4a20f3c2ff4dee4
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-03-13 12:20:09 +0000

    diwi: changes config of urls moved to portal.xml

--- app/diwi/trunk/server/src/nl/diwi/external/RouteGenerator.java
+++ app/diwi/trunk/server/src/nl/diwi/external/RouteGenerator.java
@@ -3,8 +3,8 @@ package nl.diwi.external;
 import nl.diwi.util.Constants;
 import nl.justobjects.jox.dom.JXElement;
 import nl.justobjects.jox.parser.JXBuilder;
+import org.keyworx.amuse.core.Amuse;
 import org.keyworx.oase.api.Record;
-import org.keyworx.server.ServerConfig;
 import org.postgis.Point;
 
 import java.net.MalformedURLException;
@@ -12,7 +12,7 @@ import java.net.URL;
 
 public class RouteGenerator implements Constants {
 	
-	private static String generatorUrl = ServerConfig.getProperty(GENERATOR_URL);
+	private static String generatorUrl = Amuse.server.getPortal().getProperty(GENERATOR_URL);
 		
 	//Query by example? non filled values are 'don't care'
 	public static JXElement generateRoute(Record [] prefs) {
