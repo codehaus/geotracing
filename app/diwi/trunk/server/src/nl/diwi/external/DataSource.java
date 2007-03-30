754c3fd40d38a6f334a38ecc23762db7e9cbbb9c
rlenz <rlenz@fd817712-dc16-0410-9400-acf3549a053d>
2007-03-30 07:56:52 +0000

    test media

--- app/diwi/trunk/server/src/nl/diwi/external/DataSource.java
+++ app/diwi/trunk/server/src/nl/diwi/external/DataSource.java
@@ -1,231 +1,160 @@
 package nl.diwi.external;
 
+import org.keyworx.utopia.core.util.Oase;
+import org.keyworx.utopia.core.data.UtopiaException;
+import org.keyworx.common.log.Log;
+import org.keyworx.common.log.Logging;
+import org.keyworx.amuse.core.Amuse;
+import org.keyworx.oase.api.Record;
 import nl.diwi.logic.POILogic;
 import nl.diwi.logic.RouteLogic;
+import nl.diwi.util.NetConnection;
 import nl.diwi.util.Constants;
 import nl.justobjects.jox.dom.JXElement;
 import nl.justobjects.jox.parser.JXBuilder;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.keyworx.amuse.core.Amuse;
-import org.keyworx.common.log.Log;
-import org.keyworx.common.log.Logging;
-import org.keyworx.common.net.NetUtil;
-import org.keyworx.oase.api.Record;
-import org.keyworx.utopia.core.data.UtopiaException;
-import org.keyworx.utopia.core.util.Oase;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.Properties;
 import java.util.Vector;
+import java.util.Properties;
 
 /**
- * KICHDataSource provides data that is available from the KICH database like
- * fixed routes, points of interest etc.
- * <p/>
- * It is not our intention to copy the whole KICH database into our database but only what we need to service
- * currently active clients. Data is provided either from local database or remote system, the calling party
- * should not care about this.
- * <p/>
- * Maybe all these sync methods should have a point/radius argument. 'struinen' is defenitely range limited.
- * Maybe this goes for everything?
- *
- * @author hspeijer
+ * Created by IntelliJ IDEA.
+ * User: ronald
+ * Date: Mar 29, 2007
+ * Time: 12:07:46 PM
+ * To change this template use File | Settings | File Templates.
  */
-
 public class DataSource implements Constants {
+    private Oase oase;
+        private Log log = Logging.getLog("DataSource");
 
-	private Oase oase;
-	private Log log = Logging.getLog("DataSource");
+        public DataSource(Oase oase) {
+            this.oase = oase;
+        }
 
-	public DataSource(Oase oase) {
-		this.oase = oase;
-	}
-
-	/*// Purely to catch the scenario where a generated route includes a location which is not known yet 
-	public Record syncLocation(String uri) {
-		Record route = null;
-		try {
-			route = oase.getModifier().create(LOCATION_TABLE);
-		} catch (OaseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
-	
-		return route;
-	}*/
-
-	// This request is required to get all points of interest for 'struinen'
-	public void syncPOIs() throws UtopiaException {
-		log.info("Sync Pois requested");
-		try{
-            POILogic logic = new POILogic(oase);
-            String kichRESTUrl = Amuse.server.getPortal().getProperty(KICH_REST_URL);
-
-            /*
-            <pois>
-                <poi>
-                    <id></id>
-                    <name><name>
-                    <description></description>
-                    <category></category>
-                    <x></x>
-                    <y></y>
-                    <media>
-                        <kich-uri><kich-uri>
-                        <kich-uri><kich-uri>
-                    </media>
-                </poi>
-            </pois>
-             */
-
-            /*JXElement poisElm = getXMLFromREST(kichRESTUrl);
-            if (poisElm == null) throw new UtopiaException("No results from the KICH DB");*/
-            // TODO: only done for test purposes
-
-            
-            URL url = new URL(Amuse.server.getPortal().getProperty(GENERATOR_URL) +  "?sync");
-            log.info("url " + url);
-            JXElement poisElm = new JXBuilder().build(url);
-            if(poisElm!=null){
-                log.info("poisElm " + new String(poisElm.toBytes(false), "UTF-8"));
-                Vector poiElms = poisElm.getChildrenByTag(POI_ELM);
-                for (int i = 0; i < poiElms.size(); i++) {
-                    JXElement poiElm = (JXElement) poiElms.elementAt(i);
-                    Record poi = logic.getRecord(poiElm.getChildText(ID_FIELD));
-                    if (poi != null) {
-                        logic.updateForSync(poi, poiElm);
-                    } else {
-                        logic.insertForSync(poiElm);
+        // This request is required to get all points of interest for 'struinen'
+        public void syncPOIs() throws UtopiaException {
+            try{
+                POILogic logic = new POILogic(oase);
+                String kichRESTUrl = Amuse.server.getPortal().getProperty(KICH_REST_URL);
+                kichRESTUrl += "?request=GetPois";
+                /*
+                <pois>
+                    <poi>
+                        <id></id>
+                        <name><name>
+                        <description></description>
+                        <category></category>
+                        <x></x>
+                        <y></y>
+                        <media>
+                            <kich-uri><kich-uri>
+                            <kich-uri><kich-uri>
+                        </media>
+                    </poi>
+                </pois>
+                 */
+                JXElement poisElm = NetConnection.getXMLFromREST(kichRESTUrl);
+                if(poisElm!=null){
+                    Vector poiElms = poisElm.getChildrenByTag(POI_ELM);
+                    for (int i = 0; i < poiElms.size(); i++) {
+                        JXElement poiElm = (JXElement) poiElms.elementAt(i);
+                        Record poi = logic.getRecord(poiElm.getChildText(ID_FIELD));
+                        if (poi != null) {
+                            logic.updateForSync(poi, poiElm);
+                        } else {
+                            logic.insertForSync(poiElm);
+                        }
                     }
                 }
+            }catch(Throwable t){
+                throw new UtopiaException(t);
             }
-        }catch(Throwable t){
-        	log.error("Could not sync", t);
-            throw new UtopiaException(t);
-        }
-    }
-
-	public void syncFixedRoutes() throws UtopiaException{
-        try{
-            RouteLogic logic = new RouteLogic(oase);
-            /*
-            String url = Amuse.server.getPortal().getProperty(KICH_REST_URL);
-            url += "?action=getroutes";
-            JXElement result = getXMLFromREST(url);
-            if(result == null) throw new UtopiaException("No routes from KICH");*/
-
-            // TODO: only done for test purposes
-            JXElement result = new JXElement("routes");
-            result.addChild(new JXBuilder().build(Amuse.server.getPortal().getProperty(GENERATOR_URL) + "/../testreponse/generateroute1.xml"));
-            result.addChild(new JXBuilder().build(Amuse.server.getPortal().getProperty(GENERATOR_URL) + "/../testreponse/generateroute2.xml"));
-
-            Vector routes = result.getChildrenByTag(ROUTE_ELM);
-            for(int i=0;i<routes.size();i++){
-                logic.insertRoute((JXElement)routes.elementAt(i));
-            }
-        }catch(Throwable t){
-
         }
 
-	}
+        public void syncFixedRoutes() throws UtopiaException{
+            try{
+                RouteLogic logic = new RouteLogic(oase);
+                String routingServletUrl = Amuse.server.getPortal().getProperty(ROUTING_SERVLET_URL);
+                String url = routingServletUrl + "?request=GetPredefinedRouteList";
+                JXElement routesResultElm = NetConnection.getXMLFromREST(url);
+                log.info("$$$$$$$$ " + routesResultElm);
+                /*
+                <routes>
+                  <route>
+                    <id>
+                      12345
+                    </id>
+                    <description>
+                      Rondje om de kerk
+                    </description>
+                  </route>
+                </routes> */
+                if(routesResultElm!=null & routesResultElm.getChildrenByTag(ROUTE_ELM).size()>0){
+                    Vector routes = routesResultElm.getChildrenByTag(ROUTE_ELM);
+                    for(int i=0;i<routes.size();i++){
+                        JXElement routeElm = (JXElement)routes.elementAt(i);
+                        log.info("$$$$$$$$ " + routeElm);
+                        String id = routeElm.getChildText(ID_FIELD);
+                        url = routingServletUrl + "?request=GetPredefinedRoute&RouteID=" + id;
+                        JXElement routeResultElm = NetConnection.getXMLFromREST(url);
+                        /*<gpx>
+                            <bounds minlon="152779" minlat="453239" maxlon="153925" maxlat="455055" />
+                            <name><![CDATA[route 1]]></name>
+                            <desc><![CDATA[test route 1 description]]></desc>
+                            <wpt lon="152781" lat="453254">
+                                <name>28_508983</name>
+                            </wpt>
+                            <rte>
+                                <rtept lon="153925" lat="453397"></rtept>
+                                <rtept lon="153915" lat="453406"></rtept>
+                                <rtept lon="153901" lat="453448"></rtept>
+                                <rtept lon="153879" lat="453513"></rtept>
+                                <rtept lon="153877" lat="453520"></rtept>
+                                <rtept lon="153861" lat="453562"></rtept>
+                                <rtept lon="153836" lat="453624"></rtept>
+                                <rtept lon="153810" lat="453678"></rtept>
+                                <rtept lon="153802" lat="453688"></rtept>
+                                <rtept lon="153780" lat="453699"></rtept>
+                            </rte>
+                        </gpx>*/
+                        if(routeResultElm == null) throw new UtopiaException("No route found with id " + id);
+                        logic.insertRoute(routeResultElm, ROUTE_TYPE_FIXED);
+                    }
+                }
+            }catch(Throwable t){
+                log.error("Exception in syncFixedRoutes:" + t.toString());
+                throw new UtopiaException("Exception in syncFixedRoutes", t);
+            }
+        }
 
-	// not needed all poi's are synced in one go
+        /**
+         * <media>
+         * <medium>
+         * <naam></naam>
+         * <description></description>
+         * <uri></uri>
+         * </medium>
+         * </media>
+         *
+         * @return
+         */
+        public JXElement getKICHMedia() {
+            String kichRestUrl = Amuse.server.getPortal().getProperty(KICH_REST_URL);
+            kichRestUrl += "?action=getmedia";
+            return NetConnection.getXMLFromREST(kichRestUrl);
+        }
 
-	/*public Vector syncStartLocations() {
-		return new Vector();		
-	}
-
-	public Vector syncEndLocations() {
-		return new Vector();		
-	}*/
-
-	/**
-	 * <media>
-	 * <medium>
-	 * <naam></naam>
-	 * <description></description>
-	 * <uri></uri>
-	 * </medium>
-	 * </media>
-	 *
-	 * @return
-	 */
-	public JXElement getKICHMedia() {
-		String kichRestUrl = Amuse.server.getPortal().getProperty(KICH_REST_URL);
-		kichRestUrl += "?action=getmedia";
-		return getXMLFromREST(kichRestUrl);
-	}
-
-	// creates, updates and deletes a poi in KICH
-	public String cudPOI(String anAction, JXElement aPOIElement) throws UtopiaException {
-		String kichPostUrl = Amuse.server.getPortal().getProperty(KICH_POST_URL);
-		Properties postParams = new Properties();
-		postParams.setProperty("action", anAction);
-		postParams.setProperty("xml", aPOIElement.toEscapedString());
-		//return postData(kichPostUrl, postParams);
-		postData(kichPostUrl, postParams);
-		// TODO: change this later - only for testing purposes
-		return new String("1261265");
-	}
-
-	private String postData(String aPostUrl, Hashtable thePostParams) throws UtopiaException {
-		HttpClient client = new HttpClient();
-		//client.getParams().setParameter("http.useragent", "Test Client");
-		BufferedReader br = null;
-		PostMethod method = new PostMethod(aPostUrl);
-		method.setParameter("http.useragent", "KICH Client");
-
-		if (thePostParams != null) {
-			Enumeration elements = thePostParams.keys();
-			while (elements.hasMoreElements()) {
-				String name = (String) elements.nextElement();
-				String value = (String) thePostParams.get(name);
-				method.addParameter(name, value);
-			}
-		}
-
-		StringBuffer sb = new StringBuffer();
-		try {
-			int returnCode = client.executeMethod(method);
-			if (returnCode != HttpStatus.SC_OK) {
-				throw new UtopiaException("Posting to " + aPostUrl + " was unsuccesfull! - http status:" + returnCode);
-			}
-
-			br = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream()));
-			String readLine;
-			while (((readLine = br.readLine()) != null)) {
-				sb.append(readLine);
-			}
-
-		} catch (Throwable t) {
-			log.error(t.toString());
-			throw new UtopiaException("Exception in posting data to " + aPostUrl + ": " + t.toString());
-		} finally {
-			method.releaseConnection();
-			if (br != null) try {
-				br.close();
-			} catch (Exception fe) {
-			}
-		}
-
-		return sb.toString();
-
-	}
-
-	private JXElement getXMLFromREST(String aRESTUrl) {
-		try {
-			return new JXBuilder().build(NetUtil.fetchURL(aRESTUrl));
-		} catch (Throwable t) {
-			log.error("Exception retrieving xml data from " + aRESTUrl, t);
-			return null;
-		}
-	}
-
+        // creates, updates and deletes a poi in KICH
+        public String cudPOI(String anAction, JXElement aPOIElement) throws UtopiaException {
+            String kichPostUrl = Amuse.server.getPortal().getProperty(KICH_POST_URL);
+            Properties postParams = new Properties();
+            postParams.setProperty("action", anAction);
+            postParams.setProperty("xml", aPOIElement.toEscapedString());
+            //return postData(kichPostUrl, postParams);
+            NetConnection.postData(kichPostUrl, postParams);
+            // TODO: change this later - only for testing purposes
+            return new String("1261265");
+        }
 
 }
