7e66be177fef6d74f150b2bd159a9414356363cd
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-04-05 16:09:12 +0000

    base: add utils to PGUtil

--- base/trunk/server/src/org/geotracing/gis/PostGISUtil.java
+++ base/trunk/server/src/org/geotracing/gis/PostGISUtil.java
@@ -3,24 +3,17 @@
 
 package org.geotracing.gis;
 
-import org.keyworx.oase.api.OaseException;
-import org.keyworx.oase.api.TableDef;
-import org.keyworx.oase.api.FieldDef;
-import org.keyworx.oase.api.Record;
-import org.keyworx.oase.store.source.PostgreSQLDBSource;
-import org.keyworx.oase.store.record.RecordImpl;
-import org.keyworx.oase.config.TypeDef;
-import org.keyworx.oase.config.StoreContextConfig;
-import org.keyworx.oase.util.Log;
+import nl.justobjects.jox.dom.JXElement;
+import org.keyworx.common.log.Logging;
+import org.keyworx.common.log.Log;
+
+import org.postgis.LineString;
 import org.postgis.PGgeometryLW;
 import org.postgis.Point;
 
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.util.List;
+import java.util.Vector;
 
 /**
  * Utility functions for PostGIS spatial database.
@@ -46,7 +39,7 @@ public class PostGISUtil {
 	public static final int SRID_RD = 28992;
 	public static final int SRID_WGS84 = 4326;
 	public static final int SRID_DEFAULT = SRID_WGS84;
-
+	public static Log log = Logging.getLog("PostGISUtil");
 	/**
 	 * Create PostGIS geometry object wrapped with Point.
 	 */
@@ -125,5 +118,121 @@ public class PostGISUtil {
 			throw new IllegalArgumentException("speed for this SRID not (yet) supported");
 		}
 	}
+
+	/**
+	 * Convert GPX file to linestring.
+	 *
+	 * @param gpx GPX doc
+	 * @return a PG LineString
+	 */
+	public static LineString GPXTrack2LineString(JXElement gpx) {
+		try {
+			// Create new Track object
+			Vector trkElms = gpx.getChildrenByTag("trk");
+
+			List points = new ArrayList();
+			Point point;
+			LineString lineString;
+			for (int i = 0; i < trkElms.size(); i++) {
+				JXElement nextTrk = (JXElement) trkElms.elementAt(i);
+				Vector nextTrkSegs = nextTrk.getChildrenByTag("trkseg");
+				if (nextTrkSegs == null || nextTrkSegs.size() == 0) {
+					log.info("No track segments found");
+					continue;
+				}
+
+				// Parse and handle all track segments in current track
+				for (int j = 0; j < nextTrkSegs.size(); j++) {
+					JXElement nextSeg = (JXElement) nextTrkSegs.elementAt(j);
+					Vector nextTrkPts = nextSeg.getChildrenByTag("trkpt");
+					if (nextTrkPts == null || nextTrkPts.size() == 0) {
+						log.info("No track points found");
+						continue;
+					}
+
+					// Parse and handle all track points in current track segment
+					for (int k = 0; k < nextTrkPts.size(); k++) {
+						JXElement nextTrkPt = (JXElement) nextTrkPts.elementAt(k);
+
+						// Lat/lon
+						double lon = nextTrkPt.getDoubleAttr("lon");
+						double lat = nextTrkPt.getDoubleAttr("lat");
+						double ele = 0.0d;
+						// Height (elevation)
+						String eleStr = nextTrkPt.getChildText("ele");
+						if (eleStr != null) {
+							ele = Double.parseDouble(eleStr);
+						}
+
+						point = new Point(lon, lat, ele);
+						point.setSrid(SRID_DEFAULT);
+						points.add(point);
+					}
+				}
+			}
+
+			lineString = new LineString((Point[]) points.toArray(new Point[points.size()]));
+			lineString.setSrid(SRID_DEFAULT);
+			return lineString;
+
+
+		} catch (Throwable t) {
+			log.info("Error converting GPX to linestring: " + t);
+			t.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public static LineString GPXRoute2LineString(JXElement gpx) {
+		try {
+			// Create new Track object
+			Vector routeElements = gpx.getChildrenByTag("rte");
+
+			List points = new ArrayList();
+			Point point;
+			LineString lineString;
+
+			for (int i = 0; i < routeElements.size(); i++) {
+				JXElement nextRoute = (JXElement) routeElements.elementAt(i);
+				Vector pointElements = nextRoute.getChildrenByTag("rtept");
+				if (pointElements == null || pointElements.size() == 0) {
+					log.info("No route points found");
+					continue;
+				}
+
+				// Parse and handle all routepoints
+				for (int j = 0; j < pointElements.size(); j++) {
+					JXElement nextPoint = (JXElement) pointElements.elementAt(j);
+
+					// Lat/lon
+					double lon = nextPoint.getDoubleAttr("lon");
+					double lat = nextPoint.getDoubleAttr("lat");
+					double ele = 0.0d;
+					// Height (elevation)
+					String eleStr = nextPoint.getChildText("ele");
+					if (eleStr != null) {
+						ele = Double.parseDouble(eleStr);
+					}
+
+					point = new Point(lon, lat, ele);
+					point.setSrid(SRID_DEFAULT);
+					points.add(point);
+				}
+			}
+
+			lineString = new LineString((Point[]) points.toArray(new Point[points.size()]));
+			lineString.setSrid(SRID_DEFAULT);
+			return lineString;
+
+
+		} catch (Throwable t) {
+			log.info("Error converting GPX to linestring: " + t);
+			t.printStackTrace();
+		}
+
+		return null;
+	}
+
 }
 
