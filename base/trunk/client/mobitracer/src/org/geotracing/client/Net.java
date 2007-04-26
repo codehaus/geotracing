61fa21859a7e0589909f31bc136e8a284e27486e
rlenz <rlenz@fd817712-dc16-0410-9400-acf3549a053d>
2007-04-26 09:25:14 +0000

    added extra uploadMedium

--- base/trunk/client/mobitracer/src/org/geotracing/client/Net.java
+++ base/trunk/client/mobitracer/src/org/geotracing/client/Net.java
@@ -310,8 +310,30 @@ public class Net {
 		return rsp;
 	}
 
+    public JXElement uploadMedium(String aName, String aDescription, String aType, String aMime, long aTime, byte[] theData, boolean encode) {
+		HTTPUploader uploader = new HTTPUploader();
+		JXElement rsp = null;
+		try {
+			uploader.connect(url + "/media.srv");
+			if (aName == null || aName.length() == 0) {
+				aName = "unnamed " + aType;
+			}
 
-	public JXElement utopiaReq(JXElement aReq) {
+			uploader.writeField("agentkey", agentKey);
+			uploader.writeField("name", aName);
+			uploader.writeField("description", aDescription);
+			uploader.writeFile(aName, aMime, "mt-upload", theData);
+
+			rsp = uploader.getResponse();
+
+        } catch (Throwable t) {
+			Log.log("Upload err: " + t);
+		}
+		return rsp;
+	}
+
+
+    public JXElement utopiaReq(JXElement aReq) {
 		if (kwClient == null) {
 			start();
 		}
