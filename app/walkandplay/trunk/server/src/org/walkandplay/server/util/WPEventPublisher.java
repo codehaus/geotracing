b8f78014c1c4c15e44b15a942220c63aa92a15fa
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-05-02 16:31:23 +0000

    wnp: finalize gameplay and game view services

--- app/walkandplay/trunk/server/src/org/walkandplay/server/util/WPEventPublisher.java
+++ app/walkandplay/trunk/server/src/org/walkandplay/server/util/WPEventPublisher.java
@@ -12,6 +12,9 @@ import org.keyworx.utopia.core.data.Person;
 import org.keyworx.utopia.core.util.Oase;
 import org.postgis.Point;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Publishes all events to clients using Pushlet Publisher.
  *
@@ -41,7 +44,8 @@ public class WPEventPublisher {
 	public static final String GAME_PLAY_SUBJECT = PUSHLET_SUBJECT + "/play/";
 	public static final String GAME_ROUND_SUBJECT = PUSHLET_SUBJECT + "/round/";
 	public static final String FIELD_EVENT = "event";
-	public static final String FIELD_ANSWER = "id";
+	public static final String FIELD_ANSWER = "answer";
+	public static final String FIELD_ANSWER_STATE= "answerstate";
 	public static final String FIELD_ID = "id";
 	public static final String FIELD_USER_ID = "userid";
 	public static final String FIELD_TARGET = "target";
@@ -68,21 +72,21 @@ public class WPEventPublisher {
 	public static final String FIELD_SCORE = "score";
 
 
-
 	protected static Log log;
+
 	static {
 		log = Logging.getLog("eventpublisher");
 	}
 
-		/**
+	/**
 	 * Publish task hit event to Pushlet framework.
 	 */
-	public static void answerSubmit(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, int aTaskId, int aTaskResultId, String theAnswer) {
+	public static Event answerSubmit(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, int aTaskId, int aTaskResultId, String theAnswer, String theAnswerState) {
 
+		// Pushlet subject (topic) is e.g. "/location/piet"
+		Event event = Event.createDataEvent(GAME_PLAY_SUBJECT + aGamePlayId);
 		try {
 
-			// Pushlet subject (topic) is e.g. "/location/piet"
-			Event event = Event.createDataEvent(GAME_PLAY_SUBJECT + aGamePlayId);
 			event.setField(FIELD_EVENT, EVENT_ANSWER_SUBMIT);
 			event.setField(FIELD_USER_ID, aUserId);
 			event.setField(FIELD_USER_NAME, aUserName);
@@ -91,18 +95,21 @@ public class WPEventPublisher {
 			event.setField(FIELD_TASK_ID, aTaskId);
 			event.setField(FIELD_TASKRESULT_ID, aTaskResultId);
 			event.setField(FIELD_ANSWER, theAnswer);
+			event.setField(FIELD_ANSWER_STATE, theAnswerState);
 
 			multicast(event);
 		} catch (Throwable t) {
 			log.warn("Cannot publish taskHit", t);
 		}
+		return event;
 	}
 
 	/**
 	 * Publish comment added event.
 	 */
-	public static void commentAdd(Record aCommentRecord, Oase anOase) {
+	public static Event[] commentAdd(Record aCommentRecord, Oase anOase) {
 		int commentId = aCommentRecord.getId();
+		List events = new ArrayList(2);
 
 		try {
 			// Get account name for event subject
@@ -148,23 +155,25 @@ public class WPEventPublisher {
 
 				// Send pushlet event
 				multicast(event);
+				events.add(event);
 			}
 
 		} catch (Throwable t) {
 			log.warn("Cannot publish event " + EVENT_COMMENT_ADD + " for commentId=" + commentId, t);
 		}
+		return (Event[]) events.toArray(new Event[events.size()]);
 	}
 
 
 	/**
 	 * Publish task hit event to Pushlet framework.
 	 */
-	public static void mediumHit(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, int aMediumId, int aMediumResultId) {
+	public static Event mediumHit(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, int aMediumId, int aMediumResultId) {
+		// Pushlet subject (topic) is e.g. "/location/piet"
+		Event event = Event.createDataEvent(GAME_PLAY_SUBJECT + aGamePlayId);
 
 		try {
 
-			// Pushlet subject (topic) is e.g. "/location/piet"
-			Event event = Event.createDataEvent(GAME_PLAY_SUBJECT + aGamePlayId);
 			event.setField(FIELD_EVENT, EVENT_MEDIUM_HIT);
 			event.setField(FIELD_USER_ID, aUserId);
 			event.setField(FIELD_USER_NAME, aUserName);
@@ -177,17 +186,19 @@ public class WPEventPublisher {
 		} catch (Throwable t) {
 			log.warn("Cannot publish taskHit", t);
 		}
+		return event;
 	}
 
 	/**
 	 * Publish user starts gameplay.
 	 */
-	public static void playFinish(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId) {
+	public static Event playFinish(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId) {
+
+		// Pushlet subject (topic) is e.g. "/location/piet"
+		Event event = Event.createDataEvent(GAME_ROUND_SUBJECT + aGameRoundId);
 
 		try {
 
-			// Pushlet subject (topic) is e.g. "/location/piet"
-			Event event = Event.createDataEvent(GAME_ROUND_SUBJECT + aGameRoundId);
 			event.setField(FIELD_EVENT, EVENT_PLAY_FINISH);
 			event.setField(FIELD_USER_ID, aUserId);
 			event.setField(FIELD_USER_NAME, aUserName);
@@ -198,17 +209,18 @@ public class WPEventPublisher {
 		} catch (Throwable t) {
 			log.warn("Cannot publish playFinish", t);
 		}
+		return event;
 	}
 
- 	/**
+	/**
 	 * Publish user starts gameplay.
 	 */
-	public static void playStart(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId) {
+	public static Event playStart(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId) {
+		// Pushlet subject (topic) is e.g. "/location/piet"
+		Event event = Event.createDataEvent(GAME_ROUND_SUBJECT + aGameRoundId);
 
 		try {
 
-			// Pushlet subject (topic) is e.g. "/location/piet"
-			Event event = Event.createDataEvent(GAME_ROUND_SUBJECT + aGameRoundId);
 			event.setField(FIELD_EVENT, EVENT_PLAY_START);
 			event.setField(FIELD_USER_ID, aUserId);
 			event.setField(FIELD_USER_NAME, aUserName);
@@ -219,17 +231,18 @@ public class WPEventPublisher {
 		} catch (Throwable t) {
 			log.warn("Cannot publish playStart", t);
 		}
+		return event;
 	}
 
 	/**
 	 * Publish new user location to Pushlet framework.
 	 */
-	public static void taskDone(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, int aTaskId, int aTaskResultId, int aScore) {
+	public static Event taskDone(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, int aTaskId, int aTaskResultId, int aScore) {
+		// Pushlet subject (topic) is e.g. "/location/piet"
+		Event event = Event.createDataEvent(GAME_ROUND_SUBJECT + aGameRoundId);
 
 		try {
 
-			// Pushlet subject (topic) is e.g. "/location/piet"
-			Event event = Event.createDataEvent(GAME_ROUND_SUBJECT + aGameRoundId);
 			event.setField(FIELD_EVENT, EVENT_TASK_DONE);
 			event.setField(FIELD_USER_ID, aUserId);
 			event.setField(FIELD_USER_NAME, aUserName);
@@ -243,18 +256,19 @@ public class WPEventPublisher {
 		} catch (Throwable t) {
 			log.warn("Cannot publish taskDone", t);
 		}
+		return event;
 	}
 
 
 	/**
 	 * Publish task hit event to Pushlet framework.
 	 */
-	public static void taskHit(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, int aTaskId, int aTaskResultId) {
+	public static Event taskHit(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, int aTaskId, int aTaskResultId) {
+		// Pushlet subject (topic) is e.g. "/location/piet"
+		Event event = Event.createDataEvent(GAME_PLAY_SUBJECT + aGamePlayId);
 
 		try {
 
-			// Pushlet subject (topic) is e.g. "/location/piet"
-			Event event = Event.createDataEvent(GAME_PLAY_SUBJECT + aGamePlayId);
 			event.setField(FIELD_EVENT, EVENT_TASK_HIT);
 			event.setField(FIELD_USER_ID, aUserId);
 			event.setField(FIELD_USER_NAME, aUserName);
@@ -267,42 +281,46 @@ public class WPEventPublisher {
 		} catch (Throwable t) {
 			log.warn("Cannot publish taskHit", t);
 		}
+		return event;
 	}
 
 	/**
 	 * Publish new user location to Pushlet framework.
 	 */
-	public static void userMove(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, Point aPoint) {
+	public static Event userMove(int aUserId, String aUserName, int aGameRoundId, int aGamePlayId, Point aPoint) {
+
+		Event event = Event.createDataEvent(GAME_ROUND_SUBJECT + aGameRoundId);
 
 		try {
 
 			// Pushlet subject (topic) is e.g. "/location/piet"
-			Event event = Event.createDataEvent(GAME_ROUND_SUBJECT + aGameRoundId);
 			event.setField(FIELD_EVENT, EVENT_USER_MOVE);
 			event.setField(FIELD_USER_ID, aUserId);
 			event.setField(FIELD_USER_NAME, aUserName);
 			event.setField(FIELD_GAMEROUND_ID, aGameRoundId);
 			event.setField(FIELD_GAMEPLAY_ID, aGamePlayId);
-			event.setField(FIELD_LON, aPoint.x+"");
-			event.setField(FIELD_LAT, aPoint.y+"");
+			event.setField(FIELD_LON, aPoint.x + "");
+			event.setField(FIELD_LAT, aPoint.y + "");
 
 			multicast(event);
 		} catch (Throwable t) {
 			log.warn("Cannot publish user-move", t);
 		}
+		return event;
 	}
 
 	/**
 	 * Publish Event to Pushlet framework.
 	 */
 	private static void multicast(Event anEvent) {
-		log.info("EVENT: " + anEvent.getSubject() + " " + anEvent.getField(FIELD_EVENT));
+		// log.info("EVENT: " + anEvent.getSubject() + " " + anEvent.getField(FIELD_EVENT));
 
 		// Add time attr if not present
 		if (anEvent.getField(FIELD_TIME) == null) {
 			anEvent.setField(FIELD_TIME, System.currentTimeMillis());
 		}
 		Dispatcher.getInstance().multicast(anEvent);
+
 	}
 }
 
--- app/walkandplay/trunk/server/webapp/wp/srv/myget.jsp
+++ app/walkandplay/trunk/server/webapp/wp/srv/myget.jsp
@@ -1,9 +1,9 @@
 <%@ page import="nl.justobjects.jox.dom.JXElement"%>
-<%@ page import="org.geotracing.handler.QueryHandler"%>
-<%@ page import="org.keyworx.oase.api.Record"%>
-<%@ page import="org.keyworx.plugin.tagging.logic.TagLogic"%>
+<%@ page import="org.walkandplay.server.control.GamePlayHandler"%>
 <%@ page import="javax.servlet.http.HttpServletRequest"%>
 <%@ page import="javax.servlet.http.HttpServletResponse"%>
+<%@ page import="org.keyworx.utopia.core.util.Oase"%>
+<%@ page import="org.geotracing.handler.QueryLogic"%>
 <%!
 
 	// Copyright (c) 2005 Just Objects B.V. <just@justobjects.nl>
@@ -23,65 +23,17 @@
 		try {
 		   // do application-specific command handling here
 		   // if not handled return null
-			if (command.equals(CMD_QUERY_USER_INFO)) {
-				String personId = getParameter(request, PAR_ID, null);
-				throwOnMissingParm(PAR_ID, personId);
+			if (command.equals("get-gameplay-events")) {
+				String gamePlayId = getParameter(request, PAR_ID, null);
+				throwOnMissingParm(PAR_ID, gamePlayId);
+				result = new JXElement("gameplay");
+				result.setAttr("id", gamePlayId);
 
-				Record person = oase.getFinder().read(Integer.parseInt(personId), "utopia_person");
-
-				if (person == null) {
-					throw new IllegalArgumentException("No person found for id=" + personId);
-				}
-
-				Record account = oase.getRelater().getRelated(person, "utopia_account", null)[0];
-
-				if (account == null || account.getIntField("state") != 1) {
-					throw new IllegalArgumentException("No (active) account found for person id=" + personId);
-				}
-
-				// Construct final result
-				result = Protocol.createResponse(QueryHandler.QUERY_STORE_SERVICE);
-
-				// Optional profile info
-				if (!person.isNull("extra")) {
-					JXElement extra = person.getXMLField("extra");
-
-					// Show info conditionally
-					boolean profilePublic = extra.getBoolAttr("profilepublic");
-						if (profilePublic) {
-						// We have valid person/account: create user info record custom
-						JXElement userInfo = new JXElement("record");
-
-						//userInfo.setChildText("profilepublic", profilePublic+"");
-						//userInfo.setChildText("emailpublic", emailPublic+"");
-						userInfo.setChildText("id", personId);
-
-						// Add account name
-						boolean emailPublic = extra.getBoolAttr("emailPublic");
-						userInfo.setChildText("nickname", extra.getAttr("nickname"));
-
-						if (emailPublic) {
-							userInfo.setChildText("email", account.getStringField("loginname"));
-						}
-
-						// Add optional thumb (icon) id
-						Record[] thumbRecords = oase.getRelater().getRelated(person, "base_medium", "profile");
-						if (thumbRecords.length > 0 && thumbRecords[0] != null) {
-							userInfo.setChildText("photoid", thumbRecords[0].getIdString());
-						}
-
-						// Add tags made on this person
-						String tags = new TagLogic(oase.getOaseSession()).getTagsString(person.getId(), person.getId());
-						if (tags != null && tags.length() > 0) {
-							userInfo.setChildText("tags", tags);
-						}
-
-						result.addChild(userInfo);
-					}
-				}
-
+				Oase oase = QueryLogic.getOase();
+				result.addChildren(GamePlayHandler.getGamePlayEvents(oase, Integer.parseInt(gamePlayId)));
 
 			}
+
 		} catch (Throwable t) {
 			result = new JXElement(TAG_ERROR);
 			result.setText("Unexpected Error during query " + t);
