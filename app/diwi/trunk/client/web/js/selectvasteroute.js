31d363f2cc2eb5ee2d5db3c67050eaa5db70fbe8
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-06-20 13:38:30 +0000

    diwi: more page nav

--- app/diwi/trunk/client/web/js/selectvasteroute.js
+++ app/diwi/trunk/client/web/js/selectvasteroute.js
@@ -1,57 +1,53 @@
 // JavaScript Document
 
-	var vaste_routes_names = new Array();
-	var vaste_routes_description = new Array();
-	var vaste_routes_id = new Array();
-	var vaste_routes;
-	var n_vaste_routes = -1;
-	var vaste_routes_string = '';
-	
-	var namesArray = new Array();
-	var descriptionArray = new Array();
+var vaste_routes_names = new Array();
+var vaste_routes_description = new Array();
+var vaste_routes_id = new Array();
+var vaste_routes;
+var n_vaste_routes = -1;
+var vaste_routes_string = '';
 
-	function maakVasteRoutesForm(elm) {
-		vaste_routes_string += '<p></p><form><select id="fixed_routes_form" >';
-		if (elm != null) {
-			var size = elm.length;
-			for (counter=0;counter<elm.length;counter++){
-				vaste_routes_names[counter] = elm[counter].getField('name');
-				vaste_routes_description[counter] = elm[counter].getField('description');
-				vaste_routes_id[counter] = elm[counter].id;
-			}
-			n_vaste_routes = vaste_routes_names.length;
-			var j;
-			for (i = 0; i < n_vaste_routes; i++){ 
-				j = i+1;
-				vaste_routes_string += '<option value=' + vaste_routes_id[i] + ' onClick="toonSelectedVasteRoute()">';
-				vaste_routes_string += vaste_routes_names[i];
-				vaste_routes_string += '</option>';
-			}
+var namesArray = new Array();
+var descriptionArray = new Array();
+
+function createFixedRoutesForm(records) {
+	vaste_routes_string += '<p></p><form><select id="fixed_routes_form" >';
+	if (records != null) {
+		var i;
+		for (i = 0; i < records.length; i++) {
+			vaste_routes_names[i] = records[i].getField('name');
+			vaste_routes_description[i] = records[i].getField('description');
+			vaste_routes_id[i] = records[i].id;
 		}
-		vaste_routes_string += '</select></form>';
+		n_vaste_routes = vaste_routes_names.length;
 
-		var ni = document.getElementById('vasteroutes-lb');
-		ni.innerHTML = vaste_routes_string;
-		toonSelectedVasteRoute();
-	}
-
-
-	function toonSelectedVasteRoute() {
- 		if (n_vaste_routes == -1){
-			DIWI.getFixedRoutes();
-		} else {
-			var selected = document.getElementById('fixed_routes_form').value;
-			var n = 0;
-			
-			for (i=0;i<n_vaste_routes; i++) {
-				if (selected == vaste_routes_id[i]) n = i;
-			}
-				
-			var selected_route_string = '<H1><strong>' + vaste_routes_names[n];
-			selected_route_string += '</strong><br></H1><p>'+ vaste_routes_description[n] + '</p>';
-			
-			var ni = document.getElementById('vaste_routes_content');
-			ni.innerHTML = selected_route_string;
+		for (i = 0; i < n_vaste_routes; i++) {
+			vaste_routes_string += '<option value=' + vaste_routes_id[i] + ' onClick="showFixedRoute()">';
+			vaste_routes_string += vaste_routes_names[i];
+			vaste_routes_string += '</option>';
 		}
 	}
+	vaste_routes_string += '</select></form>';
+
+	var ni = document.getElementById('vasteroutes-lb');
+	ni.innerHTML = vaste_routes_string;
+}
+
+function showFixedRoute() {
+	var selected = document.getElementById('fixed_routes_form').value;
+	var n = 0;
+
+	for (i = 0; i < n_vaste_routes; i++) {
+		if (selected == vaste_routes_id[i]) {
+			n = i;
+			break;
+		}
+	}
+
+	var selected_route_string = '<H1><strong>' + vaste_routes_names[n];
+	selected_route_string += '</strong><br></H1><p>' + vaste_routes_description[n] + '</p>';
+
+	ni = document.getElementById('vaste_routes_content');
+	ni.innerHTML = selected_route_string;
+}
 	
