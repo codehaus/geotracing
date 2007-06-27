b5c0ccdf5a57e22975622ad8f601b1e5131d9871
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-06-27 08:55:43 +0000

    diwi: web code cleanup

--- app/diwi/trunk/client/web/js/maakroute.js
+++ app/diwi/trunk/client/web/js/maakroute.js
@@ -1,5 +1,7 @@
 var ROUTE = {
 
+	sliders: new Array(),
+
 	startPOIs: null,
 	endPOIs: null,
 
@@ -25,43 +27,62 @@ var ROUTE = {
 		DH.setHTML('eindpunt-lb', formHTML);
 	},
 
+
+	createGenerateRouteForm: function () {
+		KW.CMS.getstartpoints(ROUTE.createStartPOIsForm);
+		KW.CMS.getendpoints(ROUTE.createEndPOIsForm);
+		KW.CMS.getthemes(ROUTE.createThemesForm);
+		ROUTE.createDistanceForm();
+
+	//	DH.getObject("wandelen").checked = true;
+		DIWIAPP.pr('Maak uw eigen route naar uw eigen voorkeuren! Klik op "maakroute" en er wordt een zo nauwkeurig mogelijke route voor u gemaakt, geheel naar eigen wensen. Let wel op! Wanneer u een nieuwe route maakt, wordt de vorige overschreven.');
+
+		// Create and configure the sliders
+		for (var i=1; i < 11; i++) {
+			ROUTE.createSlider('s' + i);
+		}
+	},
+
 	createRoute: function() {
 		var params = new Array();
-		params[KW.DIWI.BESLOTEN_PARAM] = s1.getValue();
-		params[KW.DIWI.HALFOPEN_PARAM] = s2.getValue();
-		params[KW.DIWI.OPEN_PARAM] = s3.getValue();
-		params[KW.DIWI.BEDRIJVEN_PARAM] = s4.getValue();
-		params[KW.DIWI.BEWONING_PARAM] = s5.getValue();
-		params[KW.DIWI.BOS_PARAM] = s6.getValue();
-		params[KW.DIWI.HEIDE_PARAM] = s7.getValue();
-		params[KW.DIWI.GRASLAND_PARAM] = s8.getValue();
-		params[KW.DIWI.ZEE_PARAM] = s9.getValue();
-		params[KW.DIWI.SLOTEN_PARAM] = s10.getValue();
+		params[KW.DIWI.BESLOTEN_PARAM] = ROUTE.sliders['s1'].getValue();
+		params[KW.DIWI.HALFOPEN_PARAM] = ROUTE.sliders['s2'].getValue();
+		params[KW.DIWI.OPEN_PARAM] = ROUTE.sliders['s3'].getValue();
+		params[KW.DIWI.BEDRIJVEN_PARAM] = ROUTE.sliders['s4'].getValue();
+		params[KW.DIWI.BEWONING_PARAM] = ROUTE.sliders['s5'].getValue();
+		params[KW.DIWI.BOS_PARAM] = ROUTE.sliders['s6'].getValue();
+		params[KW.DIWI.HEIDE_PARAM] = ROUTE.sliders['s7'].getValue();
+		params[KW.DIWI.GRASLAND_PARAM] = ROUTE.sliders['s8'].getValue();
+		params[KW.DIWI.ZEE_PARAM] = ROUTE.sliders['s9'].getValue();
+		params[KW.DIWI.SLOTEN_PARAM] = ROUTE.sliders['s10'].getValue();
+
 		params[KW.DIWI.STARTPUNT_PARAM] = document.getElementById("startpunt").value;
 		params[KW.DIWI.EINDPUNT_PARAM] = document.getElementById("eindpunt").value;
 		params[KW.DIWI.THEMA_PARAM] = document.getElementById("thema").value;
 		params[KW.DIWI.AFSTAND_PARAM] = document.getElementById("afstand").value;
-		params[KW.DIWI.WANDELAAR_PARAM] = document.getElementById("wandelen").checked;
+//		params[KW.DIWI.WANDELAAR_PARAM] = document.getElementById("wandelen").checked;
 
 		KW.DIWI.generateroute(ROUTE.onCreateRouteRsp, params);
 		DIWIAPP.pr('uw persoonlijke route wordt gegenereerd...');
 
 	},
 
-	onCreateRouteRsp: function(xmlRsp) {
-		ROUTE.generatedRouteId = xmlRsp.firstChild.getAttribute('id');
-		var route = xmlRsp.firstChild;
-		var name = route.getElementsByTagName('name')[0].firstChild.nodeValue;
-		var description = route.getElementsByTagName('description')[0].firstChild.nodeValue;
-		var routeString = '<h2>' + name + '</h2>' + description;
-		DIWIAPP.pr(routeString);
-		KW.DIWI.getmap(ROUTE.onGetRouteMapRsp, ROUTE.generatedRouteId, 580, 400);
-	},
 
-	onGetRouteMapRsp: function(xmlRsp) {
-		DIWINAV.loadPage('pages/routemap.html');
-		MAP.create();
-		MAP.addRouteLayer(ROUTE.generatedRouteId);
+	createSlider: function(id) {
+		var slider = new Slider(DH.getObject(id), DH.getObject(id + 'input'));
+		slider.setMinimum(0);
+		slider.setMaximum(100);
+		slider.setValue(0);
+
+		var sliderValue = DH.getObject(id + 'value');
+		slider.onchange = function () {
+			sliderValue.value = slider.getValue();
+		}
+
+		sliderValue.onchange = function () {
+			slider.setValue(parseInt(sliderValue.value));
+		}
+		ROUTE.sliders[id] = slider;
 	},
 
 	createStartPOIsForm: function(rspXML) {
@@ -96,6 +117,22 @@ var ROUTE = {
 		ni.innerHTML = themesString;
 	},
 
+	onCreateRouteRsp: function(xmlRsp) {
+		ROUTE.generatedRouteId = xmlRsp.firstChild.getAttribute('id');
+		var route = xmlRsp.firstChild;
+		var name = route.getElementsByTagName('name')[0].firstChild.nodeValue;
+		var description = route.getElementsByTagName('description')[0].firstChild.nodeValue;
+		var routeString = '<h2>' + name + '</h2>' + description;
+		DIWIAPP.pr(routeString);
+		KW.DIWI.getmap(ROUTE.onGetRouteMapRsp, ROUTE.generatedRouteId, 580, 400);
+	},
+
+	onGetRouteMapRsp: function(xmlRsp) {
+		DIWINAV.loadPage('pages/routemap.html');
+		MAP.create();
+		MAP.addRouteLayer(ROUTE.generatedRouteId);
+	},
+
 	rsp2Records: function(anRsp) {
 		var records = [];
 
@@ -108,99 +145,7 @@ var ROUTE = {
 	}
 }
 
-var s1,s2,s3,s4,s5,s6,s7,s8,s9,s10;
-var genRouteId;
-
-function initMakeRouteForm() {
-	KW.CMS.getstartpoints(ROUTE.createStartPOIsForm);
-	KW.CMS.getendpoints(ROUTE.createEndPOIsForm);
-	KW.CMS.getthemes(ROUTE.createThemesForm);
-	ROUTE.createDistanceForm();
-
-	DH.getObject("wandelen").checked = true;
-	DIWIAPP.pr('	Stel uw eigen route samen naar uw eigen voorkeuren! Klik op "maakroute" en er wordt een zo nauwkeurig mogelijke route voor u gemaakt, geheel naar eigen wensen. Let wel op! Wanneer u een nieuwe route maakt, wordt de vorige overschreven.');
-	s1 = new Slider(DH.getObject("slider1"), DH.getObject("s1input"));
-	s1.setMinimum(0);
-	s1.setMaximum(100);
-	s1.onchange = function () {
-		DH.getObject("s1value").value = s1.getValue();
-	};
-	s1.setValue(0);
-
-	s2 = new Slider(DH.getObject("slider2"), DH.getObject("s2input"));
-	s2.setMinimum(0);
-	s2.setMaximum(100);
-	s2.onchange = function () {
-		DH.getObject("s2value").value = s2.getValue();
-	};
-	s2.setValue(0);
-
-	s3 = new Slider(DH.getObject("slider3"), DH.getObject("s3input"));
-	s3.setMinimum(0);
-	s3.setMaximum(100);
-	s3.onchange = function () {
-		DH.getObject("s3value").value = s3.getValue();
-	};
-	s3.setValue(0);
-
-	s4 = new Slider(DH.getObject("slider4"), DH.getObject("s4input"));
-	s4.setMinimum(0);
-	s4.setMaximum(100);
-	s4.onchange = function () {
-		DH.getObject("s4value").value = s4.getValue();
-	};
-	s4.setValue(0);
-
-	s5 = new Slider(DH.getObject("slider5"), DH.getObject("s5input"));
-	s5.setMinimum(0);
-	s5.setMaximum(100);
-	s5.onchange = function () {
-		DH.getObject("s5value").value = s5.getValue();
-	};
-	s5.setValue(0);
-
-	s6 = new Slider(DH.getObject("slider6"), DH.getObject("s6input"));
-	s6.setMinimum(0);
-	s6.setMaximum(100);
-	s6.onchange = function () {
-		DH.getObject("s6value").value = s6.getValue();
-	};
-	s6.setValue(0);
-
-	s7 = new Slider(DH.getObject("slider7"), DH.getObject("s7input"));
-	s7.setMinimum(0);
-	s7.setMaximum(100);
-	s7.onchange = function () {
-		DH.getObject("s7value").value = s7.getValue();
-	};
-	s7.setValue(0);
-
-	s8 = new Slider(DH.getObject("slider8"), DH.getObject("s8input"));
-	s8.setMinimum(0);
-	s8.setMaximum(100);
-	s8.onchange = function () {
-		DH.getObject("s8value").value = s8.getValue();
-	};
-	s8.setValue(0);
-
-	s9 = new Slider(DH.getObject("slider9"), DH.getObject("s9input"));
-	s9.setMinimum(0);
-	s9.setMaximum(100);
-	s9.onchange = function () {
-		DH.getObject("s9value").value = s9.getValue();
-	};
-	s9.setValue(0);
-
-	s10 = new Slider(DH.getObject("slider10"), DH.getObject("s10input"));
-	s10.setMinimum(0);
-	s10.setMaximum(100);
-	s10.onchange = function () {
-		DH.getObject("s10value").value = s10.getValue();
-	};
-	s10.setValue(0);
-}
-
-
+/*
 function selectWandelen() {
 	DH.getObject("wandelen").checked = true;
 	DH.getObject("fietsen").checked = false;
@@ -209,13 +154,4 @@ function selectWandelen() {
 function selectFietsen() {
 	DH.getObject("wandelen").checked = false;
 	DH.getObject("fietsen").checked = true;
-}
-
-function maakGenRoutesList(elm) {
-	generatedString = '';
-	for (i = 1; i < 5; i++) {
-		generatedString += '<p><a href=\"\">' + i + '-de route </a></p>';
-	}
-	var ni = DH.getObject('generated-routes');
-	ni.innerHTML = generatedString;
-}
+} */
