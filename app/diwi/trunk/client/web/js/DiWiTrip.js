7025890e3343c8116f891e590dcecbf62ee3a8ee
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-07-23 15:31:40 +0000

    diwi: mapstuff and js trip playback

--- app/diwi/trunk/client/web/js/DiWiTrip.js
+++ app/diwi/trunk/client/web/js/DiWiTrip.js
@@ -5,12 +5,16 @@
  */
 var TRIP = {
 	trips: null,
+	mediumPopup: null,
 
 	onShowTrip: function(rsp) {
 		DH.displayOff('triplist');
+		DIWIAPP.pr('Mijn trip ingetekend.');
+		DH.displayOn('triplistbacklink');
 		MAP.show();
 		MAP.addMarkerLayer('Mijn trip');
 		TRIP.showTrace(rsp.getElementsByTagName('pt'));
+		TRIP.showTraceMedia(rsp.getElementsByTagName('medium'));
 	},
 
 	onShowTrips: function(rsp) {
@@ -23,32 +27,88 @@ var TRIP = {
 			tripsCont += '<a onclick="TRIP.showTrip(\'' + nextTrip.getField('id') + '\');" href="#"> ' + nextTrip.getField('name') + '</a><br/>';
 		}
 		DH.setHTML('triplist', tripsCont);
+		DH.displayOn('triplist');
 		DIWIAPP.pr('Hiernaast een lijst van uw gemaakte tochten.');
 	},
 
-
-	showTrace: function(thePts) {
-		var x,y,img,w,h,pt;
+  	showTraceMedia: function(theMedia) {
+		var x,y,img,w,h,medium;
 		img = 'media/images/icon-trace.png';
 		w = 10;
 		h = 10;
+
+		for (var i = 0; i < theMedia.length; i++) {
+			medium = theMedia[i];
+			// alert('medium');
+			x = medium.getAttribute('x');
+			y = medium.getAttribute('y');
+			var marker = new OpenLayers.Marker(new OpenLayers.LonLat(x,y));
+			marker.events.register('mousedown', marker, function(evt) { TRIP.showMedium(medium); Event.stop(evt); });
+			MAP.overlays['markers'].addMarker(marker);
+			//MAP.map.addPopup(popup);
+			//MAP.addMarker(x, y, img, w, h);
+		}
+	},
+
+	showMedium: function(medium) {
+		var x = medium.getAttribute('x');
+		var	y = medium.getAttribute('y');
+		var id = medium.getAttribute('id');
+		if (TRIP.mediumPopup == null) {
+			var popup = new OpenLayers.Popup("mediumpop"+i,
+				   new OpenLayers.LonLat(x,y),
+				   new OpenLayers.Size(320,240),
+				   "here is medium id=" + id,
+				   false);
+		}
+		popup.show();
+		
+	},
+
+	showTrace: function(thePts) {
+		var x,y,img,w,h,pt,xsw=0,ysw=0,xne=0,yne=0;
+		img = 'media/images/icon-trace.png';
+		w = 10;
+		h = 10;
+
 		for (var i = 0; i < thePts.length; i++) {
 			pt = thePts[i];
 			x = pt.getAttribute('x');
 			y = pt.getAttribute('y');
+			if (i == 0) {
+				xsw = xne = x;
+				ysw = yne = y;
+			} else {
+				if (x < xsw) {
+					xsw = x;
+				} else if (x > xne) {
+					xne = x;
+				}
+				if (y < ysw) {
+					ysw = y;
+				} else if (y > yne) {
+					yne = y;
+				}
+			}
 
 			MAP.addMarker(x,y,img,w,h);
+		}
+
+		if (xsw != 0 & xne != 0 && xne != xsw) {
+			var bounds = new OpenLayers.Bounds(xsw, ysw, xne, yne);
+			MAP.map.zoomToExtent(bounds);
 		}
 	},
 
 	showTrips: function() {
-		MAP.hide();
+ 		DH.displayOff('triplistbacklink');
 		KW.DIWI.gettrips(TRIP.onShowTrips, DIWIAPP.personId);
+		MAP.hide();
 	},
 
 	showTrip: function(anId) {
 		KW.DIWI.gettrip(TRIP.onShowTrip, anId);
-		DIWIAPP.pr('Hiernaast trip #' + anId);
+		DIWIAPP.pr('Trip ophalen...');
 		return false;
 	},
 
