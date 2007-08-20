5e00b32dd647e66d8099ba39bad3d09bf0704140
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-08-20 15:07:53 +0000

    wnp: mobile check

--- app/walkandplay/trunk/client/web/index.php
+++ app/walkandplay/trunk/client/web/index.php
@@ -1,4 +1,11 @@
 <?php
+	// Detect and redirect mobile browsers
+	include("mobile-check.php");
+	if(detect_mobile_device()){
+  		header('Location:mob');
+  		exit;
+  	}
+
 	header("Location:stable");
 	//include("dev.php");
 ?>
