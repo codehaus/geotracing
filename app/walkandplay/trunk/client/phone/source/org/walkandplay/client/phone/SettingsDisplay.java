d9772f2036c2e03b583f5301a8bef1acfdcfcb5c
rlenz <rlenz@fd817712-dc16-0410-9400-acf3549a053d>
2007-08-22 15:47:37 +0000

    wnp: videodisplay update

--- app/walkandplay/trunk/client/phone/source/org/walkandplay/client/phone/SettingsDisplay.java
+++ app/walkandplay/trunk/client/phone/source/org/walkandplay/client/phone/SettingsDisplay.java
@@ -1,43 +1,38 @@
 package org.walkandplay.client.phone;
 
 import de.enough.polish.ui.StringItem;
+import de.enough.polish.ui.ClockItem;
 import de.enough.polish.util.Locale;
 import org.geotracing.client.Util;
-import org.walkandplay.client.phone.Log;
 
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Image;
 
-/**
- * MobiTracer main GUI.
- *
- * @author Just van den Broecke
- * @version $Id: TraceScreen.java 254 2007-01-11 17:13:03Z just $
- */
-
-/*public class SettingsDisplay extends Form implements CommandListener {*/
 public class SettingsDisplay extends DefaultDisplay {
     private Command SOUND_CMD;
+    private Command MEDIAPLAYER_CMD;
     private Command ACCOUNT_CMD = new Command(Locale.get("settings.Account"), Command.ITEM, 2);
     private Command VERSION_CMD = new Command(Locale.get("settings.Version"), Command.ITEM, 2);
 
     StringItem text = new StringItem("", "Choose settings from the menu to change");
+    private Image logo;
 
     public SettingsDisplay(WPMidlet aMIDlet) {
         super(aMIDlet, "Settings");
 
-        try {
+         try {
             //#ifdef polish.images.directLoad
-            Image logo = Image.createImage("/settings_icon_small.png");
+            logo = Image.createImage("/settings_icon_small.png");
             //#else
-            Image logo = scheduleImage("/settings_icon_small.png");
+            logo = scheduleImage("/settings_icon_small.png");
             //#endif
-            append(logo);
         } catch (Throwable t) {
             Log.log("Could not load the images on SettingsDisplay");
         }
+
+        append(logo);
 
         if (Util.hasSound()) {
             SOUND_CMD = new Command(Locale.get("settings.SoundOff"), Command.ITEM, 2);
@@ -45,26 +40,52 @@ public class SettingsDisplay extends DefaultDisplay {
             SOUND_CMD = new Command(Locale.get("settings.SoundOn"), Command.ITEM, 2);
         }
 
+        if (midlet.useInternalMediaPlayer()) {
+            MEDIAPLAYER_CMD = new Command(Locale.get("settings.MediaPlayerIntern"), Command.ITEM, 2);
+        } else {
+            MEDIAPLAYER_CMD = new Command(Locale.get("settings.MediaPlayerExtern"), Command.ITEM, 2);
+        }
+
         //#style formbox
         append(text);
 
-        /*DigitalClockItem clock = new DigitalClockItem("");
-        append(clock);*/
-        addCommand(SOUND_CMD);
-        addCommand(ACCOUNT_CMD);
-        addCommand(VERSION_CMD);
+        ClockItem clock = new ClockItem("");
+        //#style labelinfo
+        append(clock);
 
+        setCommands();
     }
 
-    /*
-       * The commandAction method is implemented by this midlet to
-       * satisfy the CommandListener interface and handle the Cancel action.
-       */
+    private void setCommands(){
+        addCommand(ACCOUNT_CMD);
+        addCommand(SOUND_CMD);
+        addCommand(MEDIAPLAYER_CMD);
+        addCommand(VERSION_CMD);
+    }
+
     public void commandAction(Command cmd, Displayable screen) {
         if (cmd == BACK_CMD) {
             Display.getDisplay(midlet).setCurrent(prevScreen);
         } else if (cmd == SOUND_CMD) {
             Util.toggleSound();
+            if(Util.hasSound()){
+                SOUND_CMD = new Command(Locale.get("settings.SoundOff"), Command.ITEM, 2);
+            }else{
+                SOUND_CMD = new Command(Locale.get("settings.SoundOn"), Command.ITEM, 2);
+            }
+            removeAllCommands();
+            setCommands();
+        } else if (cmd == MEDIAPLAYER_CMD) {
+            removeCommand(MEDIAPLAYER_CMD);
+            if (midlet.useInternalMediaPlayer()) {
+                midlet.getPreferences().put(WPMidlet.MEDIA_PLAYER, WPMidlet.EXTERN);
+                MEDIAPLAYER_CMD = new Command(Locale.get("settings.MediaPlayerExtern"), Command.ITEM, 2);
+            }else{
+                midlet.getPreferences().put(WPMidlet.MEDIA_PLAYER, WPMidlet.INTERN);
+                MEDIAPLAYER_CMD = new Command(Locale.get("settings.MediaPlayerIntern"), Command.ITEM, 2);
+            }
+            removeAllCommands();
+            setCommands();
         } else if (cmd == ACCOUNT_CMD) {
             new AccountDisplay(midlet);
         } else if (cmd == VERSION_CMD) {
