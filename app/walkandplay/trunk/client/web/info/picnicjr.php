40051dceb4a27bb546393cfbbb99df691f2dc6b1
just <just@fd817712-dc16-0410-9400-acf3549a053d>
2007-11-01 13:39:16 +0000

    wnp: checkin of stuff from live app

---
+++ app/walkandplay/trunk/client/web/info/picnicjr.php
@@ -0,0 +1,13 @@
+<a href="javascript:wpInfo('default.php')">home</a> | <a href="javascript:wpInfo('picnic.php')">Picnic</a> | Picnic Jr  | <a href="javascript:wpInfo('howto.php')">Howto</a>
+<h2>Walk and Play @ Picnic Jr</h2>
+<hr />
+<h2>Make a Mobile Game</h2>
+<h3>"Van wie is de tas?"</h3>
+<p>Op het Westergasterrein In het Westerpark is een tas gevonden... De tas zit op slot.  Van wie zou de tas zijn? Hoe komt de tas op het Westergasfabriek terecht? Wat zou er in de tas zitten? Tijdens de workshop make-a-moble-game gaan jongeren van 14 tot 18 jaar een antwoord geven op deze vragen.</p>
+<p>De workshop bestaat uit drie delen; create, play en view. </p>
+
+<p><strong>Create</strong> de jongeren starten met het maken de mobile game. Na een kennismaking met het Westergasterrein gaan de jongeren opdrachten digitaal op het terrein plaatsen. Na het plaatsen van de opdrachten is er een mediaroute gemaakt die met een mobiele telefoon te spelen is. De mobile game is klaar om gespeeld te worden door andere teams. </p>
+
+<p><strong>Play</strong> de jongeren spelen elkanders mediaroute. Met behulp van mobiele telefoons en GPS gaan de jongeren op zoek naar de geplaatste opdrachten. Tijdens het spel krijgen ze aanwijzingen die helpen bij het opsporen van de persoon achter de tas. Door de opdrachten goed op te lossen verdienen ze steeds een stukje code van de cijferslotcombinatie. Met de complete code kunnen ze het slot openen en het verhaal ontdekken achter de tas. </p>
+
+<p><strong>View</strong> na afloop bekijken de jongeren naar de gelopen route en gemaakte opdrachten. Samen evalueren ze de zelfgemaakte mobile games.</p>
